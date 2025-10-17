236d715c9996384f89e0b36340e9da6a3c21fc6a
Danna Sofia Colunga Blanco <danna.colungab@uanl.edu.mx>
2025-10-16 19:04:33 -0600

    Delete Calculadora de promedios directory

--- Calculadora de promedios/assets/js/decoracion.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.body.style.backgroundColor = "#faebebff";
-    document.body.style.color = "#130202ff";
-    document.body.style.fontFamily = "'Segoe UI', sans-serif";
-    document.body.style.textAlign = "center";
-    document.body.style.padding = "20px";
-});
